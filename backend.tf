71ea3564ceb9f52914d28b4a366f04ca4767f719
Akhil5344 <akhil.hrx@gmail.com>
2026-02-21 10:06:40 +0000

    Fix backend formatting

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket  = "gs://my-terraform-state-gke"
+    bucket  = "my-terraform-state-gke"
     prefix  = "terraform/state/cluster-1"
     project = "ninth-iris-485511-r4"
   }
