a08cd27983edb0356e7fbb68aacbc72b66e22230
Akhil5344 <akhil.hrx@gmail.com>
2026-02-21 10:12:59 +0000

    Fix variables formatting

---
+++ modules/gke/variables.tf
@@ -0,0 +1,19 @@
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
